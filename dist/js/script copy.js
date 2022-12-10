3b68a595ed37e8f33ad3a33a73b4a2122609dba5
Omar Shaheen <16275929+omar-shaheen@users.noreply.github.com>
2022-12-10 23:28:43 +0200

    upload before clean

--- dist/js/script copy.js
+++ dist/js/script copy.js
@@ -1,12 +1,9 @@
-// Get variables
 let userInput = document.querySelector("#username");
 let emailInput = document.querySelector("#email");
 let passInput = document.querySelector("#password");
-let password_confirmation = document.querySelector("#confirmPass");
+let confirmPassInput = document.querySelector("#confirmPass");
 
-// function to vaildate username
 function userValidate() {
-  // Regex validation for username
   let rgxUser = /^(?=.*^[a-z])(?=.*\d)(?=.*[a-z]$).{5,15}$/g;
   let userTest = rgxUser.test(userInput.value);
 
@@ -20,9 +17,7 @@ if (userInput) {
   userInput.addEventListener("input", userValidate);
 }
 
-// function to vaildate email
 function validateEmail() {
-  // Regex validation for email
   let rgxEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   let emailTest = rgxEmail.test(emailInput.value);
 
@@ -36,9 +31,7 @@ if (emailInput) {
   emailInput.addEventListener("input", validateEmail);
 }
 
-// function to vaildate password
 function validatePass() {
-  // Regex validation for password
   let rgxPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
   let passTest = rgxPass.test(passInput.value);
   if (!passTest) {
@@ -52,29 +45,28 @@ if (emailInput) {
   passInput.addEventListener("input", validatePass);
 }
 
-// function to vaildate password confirmation
 function confirmPass() {
-  if (password_confirmation.value !== passInput.value) {
+  if (confirmPassInput.value !== passInput.value) {
     document.querySelector(".err_confpass").innerHTML =
       "password not confirmed";
   } else {
     document.querySelector(".err_confpass").innerHTML = "";
   }
 }
-if (password_confirmation) {
-  password_confirmation.addEventListener("input", confirmPass);
+if (confirmPassInput) {
+  confirmPassInput.addEventListener("input", confirmPass);
 }
 
 let submitform = document.getElementById("submitform");
+if (submitform) {
+  submitform.addEventListener("submit", formSubmit);
+}
 
-// function to submit form
 function formSubmit(e) {
   e.preventDefault();
 
   const formData = new FormData(submitform);
-  // console.log([...formData]);
-
-  // fetch by using method POST
+  console.log([...formData]);
   fetch(`https://goldblv.com/api/hiring/tasks/register`, {
     method: "POST",
     headers: {
@@ -84,16 +76,15 @@ function formSubmit(e) {
   })
     .then((response) => {
       response.json();
-      console.log(response);
+      console.log(response.status);
       if (response.status == 201) {
-        localStorage.setItem("email", emailInput.value);
         Swal.fire({
-          position: "top-end",
-          icon: "success",
-          title: "Registered Successfully",
+          position: 'top-end',
+          icon: 'success',
+          title: 'Registered Successfully',
           showConfirmButton: false,
-          timer: 2000,
-        });
+          timer: 2000
+        })
         setTimeout(function () {
           location = "/success.html";
         }, 2500);
@@ -101,12 +92,9 @@ function formSubmit(e) {
     })
     .catch((err) => console.log(err));
 }
-if (submitform) {
-  submitform.addEventListener("submit", formSubmit);
-}
 
 // print email after registration
-let userEmail = document.getElementById("userEmail");
-if (userEmail) {
-  userEmail.innerHTML = localStorage.getItem("email");
-}
+// let userEmail = document.getElementById("userEmail");
+// if (userEmail) {
+//   userEmail.innerHTML = email
+// }
